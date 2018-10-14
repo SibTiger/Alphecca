7fc4d818aaebbb562b1cfbb144685d9dcf9e034b
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-10-14 14:17:13 -0500

    Upped the version to 5.0
    PowerShell supports classes (OOP), but this requires .NET 5.0 foundation.  I am toying with the idea of going OOP with this program.

--- Tools/Compiler/scripts/help.ps1
+++ Tools/Compiler/scripts/help.ps1
@@ -4,7 +4,7 @@
 
 .DESCRIPTION
     This program is a compiler for the Alphecca project.  Because the project from the development source is incompatible with ZDoom's ZIP Standards, this program is designed to transform the project hierarchy to the specific standards set from ZDoom.  In addition, this program will allow the user flexibility with how the project is to be compacted.  As ZDoom allows some flexibility in terms of compression levels and algorithms, this program will also provide the same adjustability.  After a build has been successfully compiled, the user can then merely play the newly generated build with the desired Doom Engine, such as GZDoom.
-    NOTE: This program will require .NET 4.7 minimum, 7Zip for PK7 support, and GIT for dev. builds.  Please see the project's wiki for more information.
+    NOTE: This program will require .NET 5.0 minimum, 7Zip for PK7 support, and GIT for dev. builds.  Please see the project's wiki for more information.
 
 .NOTES
     Author: Nicholas Gautier
