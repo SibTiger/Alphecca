c2fdf2ea3098c2534c34b0818d1d7a42d1f82c23
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-10-14 12:38:04 -0500

    Added a new script 'common.ps1' with FetchUserInput()
    This script will house all functions that will be commonly utilized within the entire program.  This will help to reduce redundancy in the program and make it easier to maintain the code in the later future.
    
    Added in a new function 'FetchUserInput()' which will retrieve input from the end-user.

---
+++ Tools/Compiler/scripts/common.ps1
@@ -0,0 +1,20 @@
+﻿# Fetch User Input
+# --------------------------
+# Documentation
+#    This function will get input from the user.
+# --------------------------
+# Return [String]
+#    The user's given request reflected from the provided menu or confirmation inquiry.
+# --------------------------
+function FetchUserInput()
+{
+    # Because I love Python's input prompt, we will emulate it here.
+    #  I find this to be easier on the user to unify an action from the end-user.
+    Write-Host ">>>>> " -NoNewline
+
+    # Get input from the user.
+    Set-Variable -Name "stdInput" -Value $Host.UI.ReadLine()
+
+    # Return the value as a string
+    return [string]$stdInput
+} # FetchUserInput()
