2a9e5a27bcb49f0ef48a9296f96848674f690b15
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-10-14 23:14:08 -0500

    Created Initializations sub-script
    This script will setup the global variables for the program.  This will be useful for project information.

---
+++ Tools/Compiler/scripts/Initializations.ps1
@@ -0,0 +1,46 @@
+﻿# Initializations
+# --------------------------
+# Documentation
+#     This function will declare and initialize all global variables that will be available
+#     within this program.
+# --------------------------
+function Initializations()
+{
+    # Project Information
+    InitializationsProjectData;
+} # Initializations()
+
+
+
+
+# Initialization Project Data
+# --------------------------
+# Documentation
+#     Project information
+# --------------------------
+function InitializationsProjectData()
+{
+    # Project Name (sometimes the codename)
+    Set-Variable -Name "PROJECTNAME" -Value "Alphecca" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # Project Name (short)
+    Set-Variable -Name "PROJECTNAMESHORT" -Value "Alphecca" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # Project Name (Compacted; /idgames Standards)
+    Set-Variable -Name "PROJECTNAMECOMPACT" -Value "Alphecca" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # Project Known Version
+    Set-Variable -Name "PROJECTVERSION" -Value 1 `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # Project Website
+    Set-Variable -Name "PROJECTWEBSITE" -Value "https://github.com/SibTiger/Alphecca" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # Help Documentation
+    Set-Variable -Name "PROJECTHELP" -Value "https://github.com/SibTiger/Alphecca/wiki" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+} # InitializationsProjectData()
