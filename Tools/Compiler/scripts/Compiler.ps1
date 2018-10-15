ba12bf20e63293c116cf6955323a3bc95193992b
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-10-14 23:16:36 -0500

    Created the Compiler Class script
    This is very much WIP, but this will hold all compile instructions and data variables.

---
+++ Tools/Compiler/scripts/Compiler.ps1
@@ -0,0 +1,25 @@
+﻿class Compiler
+{
+    # Member Variables
+    # =================================================
+    # =================================================
+
+
+    # Member Functions
+    # =================================================
+    # =================================================
+    
+    # Main Menu
+    # --------------------------
+    # Documentation
+    #    This function will display the main menu to the user.
+    # --------------------------
+    [void]MainMenu()
+    {
+        # Header
+        Write-Host "Compiler Main Menu`n----------------------";
+        Write-Host "Compile ";
+    } # Compiler::MainMenu()
+
+
+}
