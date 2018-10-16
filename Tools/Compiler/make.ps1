ff979770636add317c8bec7732d5808b6ed89c18
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-10-16 11:05:21 -0500

    Added return codes and comments to CreateNewScriptFile()
    Added return statements to the CreateNewScriptFile(), this will be helpful to determining wither the New-Item operation was successful or failed.
    
    Aded more documentation as to what is going on within the code.

--- Tools/Compiler/make.ps1
+++ Tools/Compiler/make.ps1
@@ -32,15 +32,21 @@ function MakeCompiler()
 #    This function merely creates the new script file that will hold all of the
 #     sub-scripts into one script file.
 # --------------------------
+# Return [int]
+#    0 = Created file successfully
+#    1 = Error occurred; vague
+# --------------------------
 function CreateNewScriptFile()
 {
+    # Try to create the file; if we are unable to - then return with an error signal.
     try
     {
         New-Item -Path $SCRIPTPATH -Name $SCRIPTFILENAME -ItemType "File" `
             -Value "# $($PROJECTNAME) Compiler was generated on: $(Get-Date)`r`n`r`n" -ErrorAction Stop | Out-Null;
-    }
+        return 0;
+    } # Try
     catch
     {
-        Write-Host "Error was thrown!";
-    }
+        return 1;
+    } # Error
 } # CreateNewScriptFile()
