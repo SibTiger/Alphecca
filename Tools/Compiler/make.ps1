b94e329ed9d8415a34d8ad24352d62c70535fbed
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-10-15 21:36:55 -0500

    Added two global vars and CreateNewScriptFile()
    Added two variables: SCRIPTFILE - which holds the absolute path of the location in which this script is running from, and SCRIPTFILENAME - which holds the new script file name.
    
    Added a new function: CreateNewScriptFile().  This will generate a new empty file on the secondary storage device within the same directory of the make.ps1 script.

--- Tools/Compiler/make.ps1
+++ Tools/Compiler/make.ps1
@@ -1,4 +1,15 @@
-﻿# Make Compiler
+﻿# Global Variables
+# --------------------------
+# Script Absolute Script Path
+Set-Variable -Name "SCRIPTPATH" -Value $(Split-Path $script:MyInvocation.MyCommand.Path) -Scope Global -Force -ErrorAction SilentlyContinue;
+# Compiler Script File Name
+Set-Variable -Name "SCRIPTFILENAME" -Value "compile.ps1" -Scope Global -Force -ErrorAction SilentlyContinue;
+# --------------------------
+
+
+
+
+# Make Compiler
 # --------------------------
 # Documentation
 #    This function will display the main menu to the user.
@@ -6,3 +17,9 @@
 function MakeCompiler()
 {
 } # MakeCompiler()
+
+
+function CreateNewScriptFile()
+{
+    New-Item -Path $SCRIPTPATH -Name $SCRIPTFILENAME -ItemType "File";
+} # CreateNewScriptFile()
