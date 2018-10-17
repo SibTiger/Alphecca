3f9932e2b395852fcdbb838f8c2ed23c7c01616c
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2018-10-17 18:02:00 -0500

    Further improved locking functionality and main()
    Properly return error signals to the Operating System (or through the terminal  shell)
    Check if the file exists before trying to lock the file.
    Provided documentation regarding the return codes.
    Provide an error message to the file was not found in the relative path.

--- Tools/Compiler/debug/LockCompile.ps1
+++ Tools/Compiler/debug/LockCompile.ps1
@@ -3,6 +3,10 @@
 # Documentation
 #    This function is our main program entry point.
 # --------------------------
+# Return [int]
+#    0 = Program finished successfully
+#    1 = Program finished with errors
+# --------------------------
 function main()
 {
     # Declarations and Initializations
@@ -11,7 +15,7 @@ function main()
     Set-Variable -Name "fileName" -Value "compile.ps1" `
         -Scope Local;
     # Path of the Compile Script that we want to lock
-    Set-Variable -Name "path" -Value "..\..\..\$($fileName)" `
+    Set-Variable -Name "path" -Value "$($PSScriptRoot)\..\..\..\$($fileName)" `
         -Scope Local;
     # File Mode
     Set-Variable -Name "mode" -Value "Open" `
@@ -23,22 +27,41 @@ function main()
     Set-Variable -Name "share" -Value "None" `
         -Scope Local;
     # ---------------------------------
+    
+    # Check if the file exists and then execute the appropriate statements.
+    if (Test-Path -Path $path)
+    {
+        # Lock the file
+        $fd = [System.IO.File]::Open($path, $mode, $access, $share);
 
-    # Lock the file
-    $fd = [System.IO.File]::Open($path, $mode, $access, $share);
+        # Tell the user that the file has been locked
+        Write-Host "$($fileName) has been locked!";
 
-    # Tell the user that the file has been locked
-    Write-Host "$($fileName) has been locked!";
+        # What for the user to press the 'enter' key.
+        Read-Host -Prompt "Press Enter key to unlock $($fileName). . .";
 
-    # What for the user to press the 'enter' key.
-    Read-Host -Prompt "Press Enter key to unlock $($fileName). . .";
+        # Unlock the file
+        $fd.close();
 
-    # Unlock the file
-    $fd.close();
+        # Close with success signal
+        return 0;
+    } # File exists
+    else
+    {
+        # Warn the user that the file was not found
+        Write-Host "<!> Unable to find $($fileName) <!>";
+        write-Host "Assure that the $($fileName) exists within the root of the project source.";
+        
+        # Close with a failure signal
+        return 1;
+    } # File does not exist
 } # main()
 
 
 
 
 # Start the program
-main; 
+$errorSignal = $(main);
+
+# Terminate the program and provide the Error Code to the OS
+exit $errorSignal;
