8b7cf15c5b449e118481013a0e28b49b1059c64e
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-01-02 22:09:42 -0600

    Added new function: InitializationProgramData()
    This function sets up the program's environment.  Presently, all other functions are deprecated and will be overhauled.

--- Tools/Compiler/scripts/Initializations.ps1
+++ Tools/Compiler/scripts/Initializations.ps1
@@ -6,12 +6,42 @@
 # --------------------------
 function Initializations()
 {
-    # Project Information
-    InitializationsProjectData;
+    # Program Information
+    InitalizationProgramData;
 
     # Directory Locations
     InitializationsDirectory;
 } # Initializations()
+
+
+
+# Initialiation Program Data
+# -------------------------------
+# Documentation:
+#  This function will initialize all variables related to the program.
+# -------------------------------
+function InitalizationProgramData()
+{
+    # Program Name
+    Set-Variable -Name "_PROGRAMNAME_" -Value "PowerShell Compact-Archive Tool" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # Version
+    Set-Variable -Name "_VERSION_" -Value "0.000001-Alpha" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # Version Name
+    Set-Variable -Name "_VERSIONNAME_" -Value "Cordis" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # Release Date
+    Set-Variable -Name "_RELEASEDATE_" -Value "DD.MM.YYYY" `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+
+    # .NET Framework Requirement
+    Set-Variable -Name "_DOTNETFRAMEWORK_" -Value 5 `
+        -Option Constant -Scope Global -ErrorAction SilentlyContinue;
+} # InitalizationProgramData()
 
 
 
