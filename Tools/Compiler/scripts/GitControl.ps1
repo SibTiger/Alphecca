5cfa7f0f00c6977e4e315087ea75658f52be5b67
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-01-04 00:02:45 -0600

    Created a new class: GitControl
    This class will help to manage the user's perferences with Git, but will also contain functions in order to perform the tasks needed.  Though, this class is still slowly expanding.

---
+++ Tools/Compiler/scripts/GitControl.ps1
@@ -0,0 +1,77 @@
+﻿# Git Control
+# ------------------------------
+# ==============================
+# ==============================
+# This class holds settings regarding the use of Git and
+#  its features, but also holds functions to perform various
+#  tasks related to using Git.
+# NOTE: In order to use git, it must first already be
+#  installed within the host system.  We are merely going to
+#  use the binary to perform the tasks we want; if the
+#  binary is not available - than nothing is used.
+
+
+class GitControl
+{
+    # Member Variables :: Properties
+    # =================================================
+    # =================================================
+    # PRIVATE (emulated)
+
+    # Update Source
+    # ---------------
+    # When true, try to update the source when possible.
+    Hidden [bool] $__updateSource;
+
+
+    # Fetch Latest Commit ID
+    # ---------------
+    # When true, try to fetch the latest commit ID.
+    Hidden [bool] $__fetchCommitID;
+
+    
+    # Fetch Changelog History
+    # ---------------
+    # When true, try to retrieve a commit history.
+    Hidden [bool] $__fetchChangelog;
+
+    
+    # Changelog History Limit
+    # ---------------
+    # The maximum commits to record in the changelog.
+    Hidden [int] $__changelogLimit;
+
+
+    # Requested Branch
+    # ---------------
+    # The source Branch to work within.
+    #  It is advisable to use the Master!
+    Hidden [string] $__sourceBranch;
+
+
+    # Fetch All Collaborators [stats]
+    # ---------------
+    # Find all collaborators and record them.
+    Hidden [bool] $__fetchContributors;
+
+
+    # Fetch Stats
+    # ---------------
+    # Record any statistical information regarding
+    #  the project.
+    Hidden [bool] $__fetchStats;
+
+
+    # Generate Report
+    # ---------------
+    # Create a report based on the operations
+    #  performed by Git.
+    Hidden [bool] $__generateReport;
+
+
+    # Report Path
+    # ---------------
+    # The absolute path to store the reports that
+    #  has been generated.
+    Hidden [string] $__reportPath;
+} # GitControl
