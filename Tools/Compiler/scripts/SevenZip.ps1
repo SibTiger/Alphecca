d6f63a37dad8989d6b60660cb1db48080338829a
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-01-04 23:47:23 -0600

    Added member variables
    Added in the member variables that will be associated with this class.

--- Tools/Compiler/scripts/SevenZip.ps1
+++ Tools/Compiler/scripts/SevenZip.ps1
@@ -20,5 +20,129 @@
 
 class SevenZip
 {
-    
+    # Member Variables :: Properties
+    # =================================================
+    # =================================================
+
+
+    #region Private Variables (emulated)
+
+    # Method
+    # ---------------
+    # The method in which to compact the archive datafile.
+    Hidden [SevenZipCompressionMethod] $__Method;
+
+
+    # Algorithm [PK3|Zip]
+    # ---------------
+    # The algorithm to use when using the Zip Method
+    Hidden [SevenZipAlgorithmZip] $__algorithmZip;
+
+
+    # Algorithm [PK7|7Zip]
+    # ---------------
+    # The algorithm to use when using the 7Zip Method
+    Hidden [SevenZipAlgorithmZip] $__algorithm7Zip;
+
+
+    # Use Multithread
+    # ---------------
+    # When true, this will allow 7Zip to use multithreaded
+    #  operations when available.
+    Hidden [bool] $__useMultithread;
+
+
+    # Compression Level
+    # ---------------
+    # How tightly to compact the files.
+    Hidden [SevenCompressionLevel] $__compressionLevel;
+
+
+    # Provide Hash
+    # ---------------
+    # Retrieve the Hash of the archive datafile.
+    Hidden [bool] $__provideHash;
+
+
+    # Verify Build
+    # ---------------
+    # Test the archive datafile to assure it is not
+    #  corrupted.
+    Hidden [bool] $__verifyBuild;
+
+
+    # Generate Report
+    # ---------------
+    # Generate a report about the archive datafile.
+    Hidden [bool] $__generateReport;
+
+
+    # Report Path
+    # ---------------
+    # Store the report at a specific directory.
+    Hidden [string] $__reportPath;
+
+    #endregion
 } # SevenZip
+
+
+
+
+# 7Zip Compression Method [ENUM]
+# -------------------------------
+# Associated with what type of 7Z Method the end-user
+#  prefers when compacting an archive datafile.
+# -------------------------------
+enum SevenZipCompressionMethod
+{
+    Zip = 0;      # Zip format [PK3]
+    SevenZip = 1; # 7Zip format [PK7]
+} # SevenZipCompressionMethod
+
+
+
+
+# 7Zip Algorithm Zip [ENUM]
+# -------------------------------
+# Associated with what type of 7Z Compression Algorithm
+#  the end-user prefers when compacting an archive
+#  datafile.
+# -------------------------------
+enum SevenZipAlgorithmZip
+{
+    Deflate = 0;  # Default
+    LZMA = 1;     # LZMA Algo.
+    BZip2 = 2;    # BZip2 Algo.
+} # SevenZipAlgorithmZip
+
+
+
+
+# 7Zip Algorithm 7Zip [ENUM]
+# -------------------------------
+# Associated with what type of 7Z Compression Algorithm
+#  the end-user prefers when compacting an archive
+#  datafile.
+# -------------------------------
+enum SevenZipAlgorithm7Zip
+{
+    LZMA2 = 0;    # Default
+    LZMA = 1;     # LZMA Algo.
+    BZip2 = 2;    # BZip2 Algo.
+    PPMd = 3;     # PPMd Algo.
+} # SevenZipAlgorithm7Zip
+
+
+
+
+# 7Zip Compression Level [ENUM]
+# -------------------------------
+# Associated with what type of compression level the
+#  end-user prefers when compacting an archive datafile.
+# -------------------------------
+enum SevenCompressionLevel
+{
+    Store = 0;    # Store [No Compression] {0}
+    Normal = 1;   # Standard compression   {5}
+    Maximum = 2;  # Maximum compression    {9}
+} # DefaultCompressionLevel
