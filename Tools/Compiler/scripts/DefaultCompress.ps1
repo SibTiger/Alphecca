37118314ef2eebe9f8864b596a327b373c2de959
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-01-04 20:59:32 -0600

    Created a new class; default compression [WIP]
    Added a new class that works with the .NET's System.IO.Compression namespace.  With this class, user's will need to have .NET 4.5 installed - though this may not be an issue as .NET 5.0+ is required to use this program.
    
    Still need to add getters and setters.

---
+++ Tools/Compiler/scripts/DefaultCompress.ps1
@@ -0,0 +1,73 @@
+﻿# Default Compress
+# ------------------------------
+# ==============================
+# ==============================
+# This class holds settings regarding the use of .NET's
+#  System.IO.Compression (aka: .NET Compression).
+#  Because this was part of .NET 4.5 version and later,
+#  it is now possible for end-user's to use the native
+#  Zip provided - without requiring external API's or
+#  3rd party applications.  This should hypothetically
+#  support all targeted versions of Windows platforms.
+# Requirements: .NET Framework 4.5
+
+
+
+
+class DefaultCompress
+{
+    # Member Variables :: Properties
+    # =================================================
+    # =================================================
+
+
+    #region Private Variables (emulated)
+
+    # Compression Level
+    # ---------------
+    # The compression level when generating an
+    #  archive datafile.
+    Hidden [DefaultCompressionLevel] $__compressionLevel;
+
+
+    # Get Hash
+    # ---------------
+    # Retrieve the Hash of the archive datafile.
+    Hidden [bool] $__getHash;
+
+
+    # Verify Build
+    # ---------------
+    # Test the archive datafile to assure it is not
+    #  corrupted.
+    Hidden [bool] $__verifyBuild;
+
+
+    # Generate Report
+    # ---------------
+    # Generate a report about the archive datafile.
+    Hidden [bool] $__generateReport;
+
+
+    # Report Path
+    # ---------------
+    # Store the report at a specific directory.
+    Hidden [string] $__reportPath;
+
+    #endregion
+} # DefaultCompress
+
+
+
+
+# Default Compression Level [ENUM]
+# -------------------------------
+# Associated with what type of compression level the
+#  end-user prefers when compacting an archive datafile.
+# -------------------------------
+enum DefaultCompressionLevel
+{
+    Optimal = 0;         # Best Compression (takes time)
+    Fastest = 1;         # Light Compression (little time)
+    NoCompression = 2;   # Store [No Compression]
+} # GitCommitType
