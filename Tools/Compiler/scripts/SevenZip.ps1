9525a09f7824c4fb273b1f1ae5933865bedbfba3
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-01-04 22:50:16 -0600

    [WIP] Created a file that holds the 7Zip class
    Still very much WIP; this class will hold all 7Zip related member information and data.

---
+++ Tools/Compiler/scripts/SevenZip.ps1
@@ -0,0 +1,24 @@
+﻿# Seven Zip
+# ------------------------------
+# ==============================
+# ==============================
+# This class holds settings regarding the use of 7Zip
+#  aswell as functions in order to perform specific
+#  operations relating to compacting archive datafiles.
+#  This is an optional setting and must be enabled by
+#  the user in order to use it.
+# NOTE: This is required in order to generate 7Z (PK7)
+#   archive data files.
+# IMPORTANT NOTE:
+#  In order to use 7Zip features in this program, the
+#   end-user must also have 7Zip installed on the host
+#   system.  If the program is not installed, then the
+#   features are not available for use.
+
+
+
+
+class SevenZip
+{
+    
+} # SevenZip
