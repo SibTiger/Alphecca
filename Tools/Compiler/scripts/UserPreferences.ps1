09d7fceb4cc0eb24f168d5e1bf4f0215edcf947e
Nicholas "Tiger" Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-01-06 23:46:49 -0600

    Created an empty template class of User Preferences.
    This class will hold the user's preferences regarding the program (that is outside from other classes); this will host the generalized settings that is not specific from or covered by the other classes.

---
+++ Tools/Compiler/scripts/UserPreferences.ps1
@@ -0,0 +1,31 @@
+﻿# User Preferences
+# ------------------------------
+# ==============================
+# ==============================
+# This class holds the User Preferences within this
+#  program.  The user's preferences will give rise
+#  to what the user wants to do with this program
+#  and perform the task as the user's demands with
+#  minimal configuration after the first initial
+#  setup.  The goal of this program is to assure
+#  that the user can merely 'click-and-forget',
+#  thus the program should NOT stand in the way
+#  of the user's tasks.  With that in mind, it
+#  is paramount that the User Preferences should
+#  provide little headache as necessary; in-fact,
+#  all of the configurations within the classes
+#  provides that same principle.
+
+
+
+
+
+class UserPreferences
+{
+    # Member Variables :: Properties
+    # =================================================
+    # =================================================
+
+
+
+} # UserPreferences
