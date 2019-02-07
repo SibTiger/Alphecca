6eb58d0c127685d945ab75cb4a98bede7761e045
Nicholas Gautier <Nicholas.Gautier.Tiger@GMail.com>
2019-02-06 21:05:38 -0600

    Removed superfluous variable '$__provideHash'
    Removed an unused and unnecessary variable "$__provideHash".

--- Tools/Compiler/scripts/SevenZip.ps1
+++ Tools/Compiler/scripts/SevenZip.ps1
@@ -65,12 +65,6 @@ class SevenZip
     Hidden [SevenCompressionLevel] $__compressionLevel;
 
 
-    # Provide Hash
-    # ---------------
-    # Retrieve the Hash of the archive datafile.
-    Hidden [bool] $__provideHash;
-
-
     # Verify Build
     # ---------------
     # Test the archive datafile to assure it is not
@@ -136,9 +130,6 @@ class SevenZip
         # Compress Level
         $this.__compressionLevel = 2;
 
-        # Provide Hash
-        $this.__provideHash = $false;
-
         # Verify Build
         $this.__verifyBuild = $true;
 
@@ -165,7 +156,6 @@ class SevenZip
             [SevenZipAlgorithm7Zip] $algorithm7Zip,
             [bool] $useMultithread,
             [SevenCompressionLevel] $compressionLevel,
-            [bool] $provideHash,
             [bool] $verifyBuild,
             [bool] $generateReport)
     {
@@ -186,9 +176,6 @@ class SevenZip
 
         # Compress Level
         $this.__compressionLevel = $compressionLevel;
-
-        # Provide Hash
-        $this.__provideHash = $provideHash;
 
         # Verify Build
         $this.__verifyBuild = $verifyBuild;
@@ -316,24 +303,6 @@ class SevenZip
     {
         return $this.__compressionLevel;
     } # GetCompressionLevel()
-
-
-
-
-   <# Get Provide Hash
-    # -------------------------------
-    # Documentation:
-    #  Returns the value of the Provide Hash variable.
-    # -------------------------------
-    # Output:
-    #  [bool] Provide Hash
-    #   the value of the Provide Hash.
-    # -------------------------------
-    #>
-    [bool] GetProvideHash()
-    {
-        return $this.__provideHash;
-    } # GetProvideHash()
 
 
 
@@ -600,32 +569,6 @@ class SevenZip
         # Successfully updated.
         return $true;
     } # SetCompressionLevel()
-
-
-
-
-   <# Set Provide Hash
-    # -------------------------------
-    # Documentation:
-    #  Sets a new value for the Provide Hash variable.
-    # -------------------------------
-    # Output:
-    #  [bool] Status
-    #   true = Success; value has been changed.
-    #   false = Failure; could not set a new value.
-    # -------------------------------
-    #>
-    [bool] SetProvideHash([bool] $newVal)
-    {
-        # Because the value is either true or false, there
-        #  really is no point in checking if the new requested
-        #  value is 'legal'.  Thus, we are going to trust the
-        #  value and automatically return success.
-        $this.__provideHash = $newVal;
-
-        # Successfully updated.
-        return $true;
-    } # SetProvideHash()
 
 
 
